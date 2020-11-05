78c295314e6e6b82b14b26d5d0ae204add07cc4e
Jusepa <43047614+Jusepa@users.noreply.github.com>
2020-11-05 21:45:31 +0300

    Added skeleton for "/search" url

---
+++ src/main/java/com/jesiontrop/workhea/controller/SearchController.java
@@ -0,0 +1,21 @@
+package com.jesiontrop.workhea.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Slf4j
+@Controller
+@RequestMapping("/search")
+public class SearchController {
+
+    @GetMapping("/vacancy")
+    public String showVacancyForm(Model model) {
+
+
+
+        return "/search/vacancy";
+    }
+}
