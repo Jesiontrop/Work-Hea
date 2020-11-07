54d4b2e636f3257f968b6313b44ca9f4364defe9
Jusepa <43047614+Jusepa@users.noreply.github.com>
2020-11-07 23:12:56 +0300

    Changed DB to MySQL
    EntityScan need because without it jpa doesn't create the tables in DB

--- src/main/java/com/jesiontrop/workhea/WorkHeaApplication.java
+++ src/main/java/com/jesiontrop/workhea/WorkHeaApplication.java
@@ -2,7 +2,9 @@ package com.jesiontrop.workhea;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
+@EntityScan("com.jesiontrop.workhea")
 @SpringBootApplication
 public class WorkHeaApplication {
 
