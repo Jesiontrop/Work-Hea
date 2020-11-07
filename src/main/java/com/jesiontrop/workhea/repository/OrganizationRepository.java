5c4d551245c7d7e952e76655b1098b563124fc77
Jusepa <43047614+Jusepa@users.noreply.github.com>
2020-11-07 23:29:39 +0300

    I cleaned the unnecessary import

--- src/main/java/com/jesiontrop/workhea/repository/OrganizationRepository.java
+++ src/main/java/com/jesiontrop/workhea/repository/OrganizationRepository.java
@@ -3,10 +3,5 @@ package com.jesiontrop.workhea.repository;
 import com.jesiontrop.workhea.model.Organization;
 import org.springframework.data.repository.CrudRepository;
 
-import javax.persistence.Id;
-import java.util.List;
-
 public interface OrganizationRepository extends CrudRepository<Organization, Long> {
-
-
 }
