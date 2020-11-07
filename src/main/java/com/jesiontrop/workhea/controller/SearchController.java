d1929fe53610dac8bdfe3fdff1d1d66ecc33cf79
Jusepa <43047614+Jusepa@users.noreply.github.com>
2020-11-07 19:34:34 +0300

    Added Controller for url "/search/vacancy""

--- src/main/java/com/jesiontrop/workhea/controller/SearchController.java
+++ src/main/java/com/jesiontrop/workhea/controller/SearchController.java
@@ -1,19 +1,42 @@
 package com.jesiontrop.workhea.controller;
 
+import com.jesiontrop.workhea.model.Offer;
+import com.jesiontrop.workhea.repository.OfferRepository;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
 
 @Slf4j
 @Controller
 @RequestMapping("/search")
 public class SearchController {
 
-    @GetMapping("/vacancy")
-    public String showVacancyForm(Model model) {
+    private final OfferRepository offerRepository;
 
+    @Autowired
+    public SearchController(OfferRepository offerRepository) {
+        this.offerRepository = offerRepository;
+    }
+
+    @GetMapping("/vacancy")
+    public String showVacancy(@RequestParam(defaultValue = "") String q, Model model) {
+        List<Offer> offers = offerRepository.findAllByVacancyTitleContains(q);
+        boolean hasSearchError = false;
+
+        final String searchError = "No results found for \"" + q + "\"";
+
+        if (offers.size() == 0)
+            hasSearchError = true;
+
+        model.addAttribute("searchError", searchError);
+        model.addAttribute("hasSearchError", hasSearchError);
+        model.addAttribute("offers", offers);
 
 
         return "/search/vacancy";
