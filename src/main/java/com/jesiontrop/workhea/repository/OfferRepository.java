c5457172b06dae91d04e9533e0f74bcc5dc2417d
Jusepa <43047614+Jusepa@users.noreply.github.com>
2020-11-07 18:27:14 +0300

    Added method
    Method find Offers by Vacancy_Title

--- src/main/java/com/jesiontrop/workhea/repository/OfferRepository.java
+++ src/main/java/com/jesiontrop/workhea/repository/OfferRepository.java
@@ -3,5 +3,9 @@ package com.jesiontrop.workhea.repository;
 import com.jesiontrop.workhea.model.Offer;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface OfferRepository extends CrudRepository<Offer, Long> {
+
+    List<Offer> findAllByVacancyTitleContains(String Query);
 }
