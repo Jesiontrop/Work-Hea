05c078848decfd9692ea504ccad0d7a050cb4f7a
Jusepa <43047614+Jusepa@users.noreply.github.com>
2020-11-08 21:00:05 +0300

    I modified method
    I replaced the standart jpa search method to MySQL fulltext search

--- src/main/java/com/jesiontrop/workhea/repository/OfferRepository.java
+++ src/main/java/com/jesiontrop/workhea/repository/OfferRepository.java
@@ -1,11 +1,17 @@
 package com.jesiontrop.workhea.repository;
 
 import com.jesiontrop.workhea.model.Offer;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface OfferRepository extends CrudRepository<Offer, Long> {
 
-    List<Offer> findAllByVacancyTitleContains(String Query);
+    @Modifying
+    @Query( value = "SELECT * FROM offer WHERE MATCH (vacancy_title) AGAINST (:q)",
+            nativeQuery = true)
+    List<Offer> findAllByVacancyTitleContains(@Param("q") String q);
 }
