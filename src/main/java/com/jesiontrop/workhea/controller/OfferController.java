ecc7daa0e85f1ba873a97c1a9b1203018bc4655d
Jusepa <43047614+Jusepa@users.noreply.github.com>
2020-11-09 23:16:44 +0300

    Added Controll for url "/offer/{id}?"

---
+++ src/main/java/com/jesiontrop/workhea/controller/OfferController.java
@@ -0,0 +1,41 @@
+package com.jesiontrop.workhea.controller;
+
+import com.jesiontrop.workhea.model.Offer;
+import com.jesiontrop.workhea.repository.OfferRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@Slf4j
+@RequestMapping("/offer")
+public class OfferController {
+
+    OfferRepository offerRepository;
+
+    @Autowired
+    public OfferController(OfferRepository offerRepository) {
+        this.offerRepository = offerRepository;
+    }
+
+    @GetMapping("/{id}")
+    public String showOffer(@PathVariable("id") Long id, Model model) {
+        Offer offer = offerRepository.findById(id).orElse(null);
+        if (offer == null)
+            return "redirect:/offer/error/notfound";
+
+        model.addAttribute("offer", offer);
+
+        return "/offer";
+    }
+
+    @GetMapping("/error/notfound")
+    public String errorNotFoundOffer() {
+
+        return "error/errorNotFoundOffer";
+    }
+}
