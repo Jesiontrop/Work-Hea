544250347837bb41f51d50e3bd6f985e2d84da88
Jusepa <43047614+Jusepa@users.noreply.github.com>
2020-11-13 23:42:53 +0300

    Added fulltext search method by name

--- src/main/java/com/jesiontrop/workhea/repository/OrganizationRepository.java
+++ src/main/java/com/jesiontrop/workhea/repository/OrganizationRepository.java
@@ -1,7 +1,17 @@
 package com.jesiontrop.workhea.repository;
 
 import com.jesiontrop.workhea.model.Organization;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface OrganizationRepository extends CrudRepository<Organization, Long> {
+
+    @Modifying
+    @Query( value = "SELECT * FROM organization WHERE MATCH (name_of_organization) AGAINST (:q)",
+            nativeQuery = true)
+    List<Organization> findAllByNameOfOrganizationContains(@Param("q") String q);
 }
