5145252a209f9e79477b5ef0fc47480d0f0551ab
Jusepa <43047614+Jusepa@users.noreply.github.com>
2020-11-19 22:37:42 +0300

    Added method findAllByNameOfOrganizationContains with pageable

--- src/main/java/com/jesiontrop/workhea/repository/OrganizationRepository.java
+++ src/main/java/com/jesiontrop/workhea/repository/OrganizationRepository.java
@@ -6,6 +6,7 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 
+import java.awt.print.Pageable;
 import java.util.List;
 
 public interface OrganizationRepository extends CrudRepository<Organization, Long> {
@@ -14,4 +15,10 @@ public interface OrganizationRepository extends CrudRepository<Organization, Lon
     @Query( value = "SELECT * FROM organization WHERE MATCH (name_of_organization) AGAINST (:q)",
             nativeQuery = true)
     List<Organization> findAllByNameOfOrganizationContains(@Param("q") String q);
+
+    @Modifying
+    @Query( value = "SELECT * FROM organization WHERE MATCH (name_of_organization) AGAINST (:q)",
+            countQuery = "SELECT count(*) FROM organization WHERE MATCH (name_of_organization) AGAINST (:q)",
+        nativeQuery = true)
+    List<Organization> findAllByNameOfOrganizationContains(@Param("q") String q, Pageable pageable);
 }
