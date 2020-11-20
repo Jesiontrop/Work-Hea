ad72dfcd08af711abdce6054284be1ae29310941
Jusepa <43047614+Jusepa@users.noreply.github.com>
2020-11-21 00:13:12 +0300

    FIxed bag

--- src/main/java/com/jesiontrop/workhea/repository/OrganizationRepository.java
+++ src/main/java/com/jesiontrop/workhea/repository/OrganizationRepository.java
@@ -1,22 +1,23 @@
 package com.jesiontrop.workhea.repository;
 
 import com.jesiontrop.workhea.model.Organization;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 
-import java.awt.print.Pageable;
 import java.util.List;
 
 public interface OrganizationRepository extends CrudRepository<Organization, Long> {
 
+    //MySql request
     @Modifying
     @Query( value = "SELECT * FROM organization WHERE MATCH (name_of_organization) AGAINST (:q)",
             nativeQuery = true)
     List<Organization> findAllByNameOfOrganizationContains(@Param("q") String q);
 
-    @Modifying
+    //MySql request
     @Query( value = "SELECT * FROM organization WHERE MATCH (name_of_organization) AGAINST (:q)",
             countQuery = "SELECT count(*) FROM organization WHERE MATCH (name_of_organization) AGAINST (:q)",
         nativeQuery = true)
