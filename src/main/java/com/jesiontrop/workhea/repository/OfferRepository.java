9cef05ebc749cf16e4aecc09a8d5fb9c48e85743
Jusepa <43047614+Jusepa@users.noreply.github.com>
2020-11-21 00:12:48 +0300

    Added findOfferByOrOrganizationIdEquals

--- src/main/java/com/jesiontrop/workhea/repository/OfferRepository.java
+++ src/main/java/com/jesiontrop/workhea/repository/OfferRepository.java
@@ -1,22 +1,33 @@
 package com.jesiontrop.workhea.repository;
 
 import com.jesiontrop.workhea.model.Offer;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 
-import java.awt.print.Pageable;
 import java.util.List;
 
 public interface OfferRepository extends CrudRepository<Offer, Long> {
 
     @Modifying
+    @Query( value = "SELECT * FROM offer WHERE organization_id = :organizationId",
+            nativeQuery = true)
+    List<Offer> findOfferByOrOrganizationIdEquals(Long organizationId);
+
+    @Query( value = "SELECT * FROM offer WHERE organization_id = :organizationId",
+            countQuery = "SELECT count(*) FROM offer WHERE organization_id = :organizationId",
+            nativeQuery = true)
+    List<Offer> findOfferByOrOrganizationIdEquals(Long organizationId, Pageable pageable);
+
+    //MySql request
+    @Modifying
     @Query( value = "SELECT * FROM offer WHERE MATCH (vacancy_title) AGAINST (:q)",
             nativeQuery = true)
     List<Offer> findAllByVacancyTitleContains(@Param("q") String q);
 
-    @Modifying
+    //MySql request
     @Query( value = "SELECT * FROM offer WHERE MATCH (vacancy_title) AGAINST (:q)",
             countQuery = "SELECT count(*) FROM offer WHERE MATCH (vacancy_title) AGAINST (:q)",
         nativeQuery = true)
