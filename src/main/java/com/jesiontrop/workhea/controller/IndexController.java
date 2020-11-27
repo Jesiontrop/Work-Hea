290d7c62491801ae15401dbc836a1a29d278c043
Jusepa <43047614+Jusepa@users.noreply.github.com>
2020-11-27 22:16:01 +0300

    Added PostMapping

--- src/main/java/com/jesiontrop/workhea/controller/IndexController.java
+++ src/main/java/com/jesiontrop/workhea/controller/IndexController.java
@@ -2,17 +2,35 @@ package com.jesiontrop.workhea.controller;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @Slf4j
 @RequestMapping("/")
 public class IndexController {
 
+    private final String isSearchVacancy = "isSearchVacancy";
+    private final String isSearchOrganization = "isSearchOrganization";
+
     @GetMapping
-    public String showIndex() {
+    public String showIndex(Model model) {
+        model.addAttribute("isSearchVacancy", isSearchVacancy);
+        model.addAttribute("isSearchOrganization", isSearchOrganization);
 
         return "index";
+    }
+
+    @PostMapping
+    public String processSearch(@RequestParam(value = "isSearch") String isSearch,
+                                @RequestParam("q") String q) {
+
+        if (isSearch.equals(isSearchVacancy))
+            return "redirect:/search/vacancy?q=" + q;
+
+        if (isSearch.equals(isSearchOrganization))
+            return "redirect:/search/organization?q=" + q;
+
+        return "redirect:/";
     }
 }
