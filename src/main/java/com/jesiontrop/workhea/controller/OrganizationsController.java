8fad429329dd8ffb5514c2d6ff0b96312dbce5bb
Jusepa <43047614+Jusepa@users.noreply.github.com>
2020-12-03 22:37:52 +0300

    Fixed bag

--- src/main/java/com/jesiontrop/workhea/controller/OrganizationsController.java
+++ src/main/java/com/jesiontrop/workhea/controller/OrganizationsController.java
@@ -14,6 +14,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Controller
@@ -69,9 +70,10 @@ public class OrganizationsController {
             return "redirect:/organizations/error/notfound";
 
         Integer pageSize = offerProps.getPageSize();
-        Integer pageStart = page > 0 ? (page - 1) * offerProps.getPageSize() : 0;
 
-        Pageable offerPageRequest = PageRequest.of(pageStart, pageSize + pageStart);
+        Pageable offerPageRequest = PageRequest.of(0, pageSize);
+        for (long i = 2; i <= page; i++)
+            offerPageRequest = offerPageRequest.next();
         List<Offer> offers = offerRepository.findOfferByOrOrganizationIdEquals(id, offerPageRequest);
         model.addAttribute("offers", offers);
 
