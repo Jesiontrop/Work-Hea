1c2870d0994f2ea5c3972e611e34ff89cc866c5d
Jusepa <43047614+Jusepa@users.noreply.github.com>
2020-12-11 22:30:55 +0300

    Update Resume.java

--- src/main/java/com/jesiontrop/workhea/model/Resume.java
+++ src/main/java/com/jesiontrop/workhea/model/Resume.java
@@ -1,4 +1,53 @@
 package com.jesiontrop.workhea.model;
 
+import com.jesiontrop.workhea.security.model.User;
+import lombok.Data;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+@Data
+@Entity
 public class Resume {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String fullName;
+    private String sex;
+    private Date dateOfBirth;
+    private String cityOfResidence;
+
+    private String phoneNumber;
+    private String workPhone;
+    private String homePhone;
+    private String email;
+    private String anotherSite;
+
+    private String careerObjective;
+
+    @ManyToMany(targetEntity = Specialization.class)
+    private List<Specialization> specializations;
+    private Integer salary;
+    @ManyToMany(targetEntity = Employment.class)
+    private List<Employment> employments;
+    @ManyToMany(targetEntity = Schedule.class)
+    private List<Schedule> schedules;
+
+    @Lob
+    @Column(length = 50000)
+    private String aboutMe;
+
+    private Date placedAt;
+
+    @PrePersist
+    void placedAt() {
+        this.placedAt = new Date();
+    }
+
+    @ManyToOne(targetEntity = User.class)
+    private User user;
 }
