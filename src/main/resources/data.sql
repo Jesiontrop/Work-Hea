a4fe11ef22e204b4278f546b65279f7d055ada5f
Jusepa <43047614+Jusepa@users.noreply.github.com>
2021-01-24 18:00:15 +0300

    Add SQL code
    Update values into tables employement, schedule, spcilization when the server restarts

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,1 +1,41 @@
+DELETE FROM employment;
+DELETE FROM schedule;
+DELETE FROM specialization;
 
+INSERT INTO employment (id, name) VALUES
+            (1, 'Full employment'),
+            (2, 'Part-time employment'),
+            (3, 'Project work'),
+            (4, 'Volunteering'),
+            (5, 'Internship');
+
+INSERT INTO schedule(id, name) VALUES
+            (1, 'Full day'),
+            (2, 'Shift work'),
+            (3, 'Flexible schedule'),
+            (4, 'Remote work'),
+            (5, 'Shift method');
+
+INSERT INTO specialization(id, name) VALUES
+            (1, 'Accounting department'),
+            (2, 'Administrative staff'),
+            (3, 'Arts, Entertainment, Mass Media'),
+            (4, 'Car business'),
+            (5, 'Construction, Architecture'),
+            (6, 'Consulting'),
+            (7, 'Extraction of raw materials'),
+            (8, 'Finance, Banking, Investments'),
+            (9, 'Information technology, Internet, Multimedia'),
+            (10, 'Insurance'),
+            (11, 'Lawyers'),
+            (12, 'Manufacturing, Technology'),
+            (13, 'Marketing, Advertising, PR'),
+            (14, 'Medicine, Pharmaceuticals'),
+            (15, 'Other'),
+            (16, 'Personnel Management'),
+            (17, 'Procurement'),
+            (18, 'Sales'),
+            (19, 'Science, Education'),
+            (20, 'Tourism, Hotels, Restaurants'),
+            (21, 'Transport, Logistics'),
+            (22, 'Working staff');
