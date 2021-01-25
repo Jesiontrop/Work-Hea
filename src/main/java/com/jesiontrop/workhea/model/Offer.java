ebc4334443fdf530b3c3b7ab5fd772d5a8910849
Jusepa <43047614+Jusepa@users.noreply.github.com>
2021-01-25 22:05:54 +0300

    Change ID GeneratinoType in offer to IDENTITY

--- src/main/java/com/jesiontrop/workhea/model/Offer.java
+++ src/main/java/com/jesiontrop/workhea/model/Offer.java
@@ -12,7 +12,7 @@ import java.util.Date;
 public class Offer {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @NotBlank(message = "Vacancy title is required")
