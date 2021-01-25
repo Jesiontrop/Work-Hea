1111c620dd026d8a5906aae37671b3049e620b3c
Jusepa <43047614+Jusepa@users.noreply.github.com>
2021-01-25 22:06:01 +0300

    Change ID GeneratinoType in organization to IDENTITY

--- src/main/java/com/jesiontrop/workhea/model/Organization.java
+++ src/main/java/com/jesiontrop/workhea/model/Organization.java
@@ -12,7 +12,7 @@ import java.util.List;
 public class Organization {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     @NotBlank(message = "Name of organization is required")
     private String nameOfOrganization;
