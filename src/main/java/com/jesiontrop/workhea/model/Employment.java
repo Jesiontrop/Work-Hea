a883010c90333cc79e95b20edba3a1e63a467154
Jusepa <43047614+Jusepa@users.noreply.github.com>
2021-01-25 22:05:47 +0300

    Change ID GeneratinoType in employment to IDENTITY

--- src/main/java/com/jesiontrop/workhea/model/Employment.java
+++ src/main/java/com/jesiontrop/workhea/model/Employment.java
@@ -12,7 +12,7 @@ import javax.persistence.Id;
 public class Employment {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String name;
