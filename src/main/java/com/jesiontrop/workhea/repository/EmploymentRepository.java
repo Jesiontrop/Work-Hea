270f81e17312980c95cbc252f0073b28d75679de
Jusepa <43047614+Jusepa@users.noreply.github.com>
2021-01-28 22:11:47 +0300

    Create EmploymentRepository.java

---
+++ src/main/java/com/jesiontrop/workhea/repository/EmploymentRepository.java
@@ -0,0 +1,7 @@
+package com.jesiontrop.workhea.repository;
+
+import com.jesiontrop.workhea.model.Employment;
+import org.springframework.data.repository.CrudRepository;
+
+public interface EmploymentRepository extends CrudRepository<Employment, Long> {
+}
