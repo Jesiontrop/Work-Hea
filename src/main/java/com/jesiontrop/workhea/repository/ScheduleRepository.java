261eab7ab5382973e38d361dee5204d075c4f526
Jusepa <43047614+Jusepa@users.noreply.github.com>
2021-01-29 23:46:55 +0300

    Create ScheduleRepository.java

---
+++ src/main/java/com/jesiontrop/workhea/repository/ScheduleRepository.java
@@ -0,0 +1,7 @@
+package com.jesiontrop.workhea.repository;
+
+import com.jesiontrop.workhea.model.Schedule;
+import org.springframework.data.repository.CrudRepository;
+
+public interface ScheduleRepository extends CrudRepository<Schedule, Long> {
+}
