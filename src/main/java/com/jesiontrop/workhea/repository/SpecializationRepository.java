177f304645ba7ad667261ef74dde4a634ef35c53
Jusepa <43047614+Jusepa@users.noreply.github.com>
2021-01-30 23:20:30 +0300

    Create SpecializationRepository.java

---
+++ src/main/java/com/jesiontrop/workhea/repository/SpecializationRepository.java
@@ -0,0 +1,7 @@
+package com.jesiontrop.workhea.repository;
+
+import com.jesiontrop.workhea.model.Specialization;
+import org.springframework.data.repository.CrudRepository;
+
+public interface SpecializationRepository extends CrudRepository<Specialization, Long> {
+}
