725f8b012b578a39128fa11a7c6c86b1aa98a6bc
Jusepa <43047614+Jusepa@users.noreply.github.com>
2021-01-31 23:44:57 +0300

    Create ResumeRepository.java

---
+++ src/main/java/com/jesiontrop/workhea/repository/ResumeRepository.java
@@ -0,0 +1,7 @@
+package com.jesiontrop.workhea.repository;
+
+import com.jesiontrop.workhea.model.Resume;
+import org.springframework.data.repository.CrudRepository;
+
+public interface ResumeRepository extends CrudRepository<Resume, Long> {
+}
