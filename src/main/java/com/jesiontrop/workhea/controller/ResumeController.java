4419c75e1c742d8438e2774faff174645c27daea
Jusepa <43047614+Jusepa@users.noreply.github.com>
2021-02-02 21:11:10 +0300

    Add @RequestMapping and @GetMapping

--- src/main/java/com/jesiontrop/workhea/controller/ResumeController.java
+++ src/main/java/com/jesiontrop/workhea/controller/ResumeController.java
@@ -1,11 +1,15 @@
 package com.jesiontrop.workhea.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/resume")
 public class ResumeController {
 
+    @GetMapping
     public String resumeForm() {
-        return null;
+        return "resume";
     }
 }
