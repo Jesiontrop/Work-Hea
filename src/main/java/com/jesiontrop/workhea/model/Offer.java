7d1547ee1b6c68ee6bafa6794ce72cece827f719
Jusepa <43047614+Jusepa@users.noreply.github.com>
2021-02-06 22:54:43 +0300

    Create Constructor

--- src/main/java/com/jesiontrop/workhea/model/Offer.java
+++ src/main/java/com/jesiontrop/workhea/model/Offer.java
@@ -30,6 +30,9 @@ public class Offer {
     @ManyToOne(targetEntity = Organization.class)
     private Organization organization;
 
+    public Offer() {
+    }
+
     public Offer(@NotBlank(message = "Vacancy title is required") String vacancyTitle, @NotNull(message = "Salary is required") Integer salary) {
         this.vacancyTitle = vacancyTitle;
         this.salary = salary;
