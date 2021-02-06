c31082d30535d8274883c1d2f345b11288c2e599
Jusepa <43047614+Jusepa@users.noreply.github.com>
2021-02-06 23:01:08 +0300

    Clear code

--- src/test/java/com/jesiontrop/workhea/repository/MySQLOrganizationRepositoryTest.java
+++ src/test/java/com/jesiontrop/workhea/repository/MySQLOrganizationRepositoryTest.java
@@ -1,20 +1,14 @@
 package com.jesiontrop.workhea.repository;
 
-import com.jesiontrop.workhea.model.Offer;
 import com.jesiontrop.workhea.model.Organization;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
-
-import javax.transaction.Transactional;
 
 import java.util.ArrayList;
 import java.util.List;
