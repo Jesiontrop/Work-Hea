abb9e26e498f04ad4d7d6097bf8b84ef1d10cfcc
Jusepa <43047614+Jusepa@users.noreply.github.com>
2021-02-06 22:55:18 +0300

    Change Generation Type

--- src/main/java/com/jesiontrop/workhea/model/Organization.java
+++ src/main/java/com/jesiontrop/workhea/model/Organization.java
@@ -12,7 +12,7 @@ import java.util.List;
 public class Organization {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.AUTO, generator = "organization_sequence")
     private Long id;
     @NotBlank(message = "Name of organization is required")
     private String nameOfOrganization;
