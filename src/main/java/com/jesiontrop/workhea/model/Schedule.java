48fb9db60794cb37810bfd04404d77d05b2d9014
Jusepa <andrey.ru754@gmail.com>
2021-03-05 22:26:53 +0300

    feat: add anotation RestResource in Shedule model

--- src/main/java/com/jesiontrop/workhea/model/Schedule.java
+++ src/main/java/com/jesiontrop/workhea/model/Schedule.java
@@ -3,14 +3,16 @@ package com.jesiontrop.workhea.model;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.data.rest.core.annotation.RestResource;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
-@Data
 @Entity
+@RestResource(rel = "schedules", path = "schedules")
+@Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class Schedule {
