5b95c14cd7d44cd5c6f351bd7a8fd04f6213ea23
Jusepa <andrey.ru754@gmail.com>
2021-03-05 22:23:31 +0300

    feat: add anotation RestResource in Employment model

--- src/main/java/com/jesiontrop/workhea/model/Employment.java
+++ src/main/java/com/jesiontrop/workhea/model/Employment.java
@@ -3,14 +3,16 @@ package com.jesiontrop.workhea.model;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.data.rest.core.annotation.RestResource;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
-@Data
 @Entity
+@RestResource(rel = "employments", path = "employments")
+@Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class Employment {
