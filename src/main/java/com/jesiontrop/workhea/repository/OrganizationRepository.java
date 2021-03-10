95cf2e58ce65aad0b1269b08ad31fea6c6e2a590
Andrey Barkov <andrey.ru754@gmail.com>
2021-03-10 21:52:12 +0300

    refactor: add RepositoryRestResource annotations

--- src/main/java/com/jesiontrop/workhea/repository/OrganizationRepository.java
+++ src/main/java/com/jesiontrop/workhea/repository/OrganizationRepository.java
@@ -2,9 +2,11 @@ package com.jesiontrop.workhea.repository;
 
 import com.jesiontrop.workhea.model.Organization;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import java.util.List;
 
+@RepositoryRestResource(collectionResourceRel = "organizations", path = "organizations")
 public interface OrganizationRepository extends CrudRepository<Organization, Long> {
 
     List<Organization> findAllByNameOfOrganizationContains(String q);
