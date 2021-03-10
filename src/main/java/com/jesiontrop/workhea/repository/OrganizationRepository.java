b780ece6a5b3c0205114ccfd8304dd97a4a0756c
Andrey Barkov <andrey.ru754@gmail.com>
2021-03-10 21:42:31 +0300

    refactor: clean up OrganizationRepository and delete test

--- src/main/java/com/jesiontrop/workhea/repository/OrganizationRepository.java
+++ src/main/java/com/jesiontrop/workhea/repository/OrganizationRepository.java
@@ -10,34 +10,4 @@ import org.springframework.data.repository.query.Param;
 import java.util.List;
 
 public interface OrganizationRepository extends CrudRepository<Organization, Long> {
-
-    Iterable<Organization> findAll(Pageable pageable);
-
-    /*
-   MySql request
-   "MATCH () AGAINST ()" only work if name_of_organization have FULLTEXT
-   else need start the request -
-   "
-       ALTER TABLE organization
-       ADD FULLTEXT(name_of_organization)
-   "
-    */
-    @Modifying
-    @Query( value = "SELECT * FROM organization WHERE MATCH (name_of_organization) AGAINST (:q)",
-            nativeQuery = true)
-    List<Organization> findAllByNameOfOrganizationContains(@Param("q") String q);
-
-    /*
-   MySql request
-   "MATCH () AGAINST ()" only work if name_of_organization have FULLTEXT
-   else need start the request -
-   "
-       ALTER TABLE organization
-       ADD FULLTEXT(name_of_organization)
-   "
-    */
-    @Query( value = "SELECT * FROM organization WHERE MATCH (name_of_organization) AGAINST (:q)",
-            countQuery = "SELECT count(*) FROM organization WHERE MATCH (name_of_organization) AGAINST (:q)",
-            nativeQuery = true)
-    List<Organization> findAllByNameOfOrganizationContains(@Param("q") String q, Pageable pageable);
 }
