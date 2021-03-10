fb911f7acad5f43f3603eaa3e485bb6c396cf9ac
Andrey Barkov <andrey.ru754@gmail.com>
2021-03-10 21:41:45 +0300

    refactor: clear up OfferRepository and delete test

--- src/main/java/com/jesiontrop/workhea/repository/OfferRepository.java
+++ src/main/java/com/jesiontrop/workhea/repository/OfferRepository.java
@@ -10,41 +10,4 @@ import org.springframework.data.repository.query.Param;
 import java.util.List;
 
 public interface OfferRepository extends CrudRepository<Offer, Long> {
-
-    Iterable<Offer> findAll(Pageable pageable);
-
-    @Modifying
-    @Query( value = "SELECT o FROM Offer o WHERE o.organization.id = :organizationId")
-    List<Offer> findOfferByOrOrganizationIdEquals(Long organizationId);
-
-    @Query( value = "SELECT o FROM Offer o WHERE o.organization.id = :organizationId")
-    List<Offer> findOfferByOrOrganizationIdEquals(Long organizationId, Pageable pageable);
-
-    /*
-    MySql request
-    "MATCH () AGAINST ()" only work if vacancy_title have FULLTEXT
-    else need start the request -
-    "
-        ALTER TABLE offer
-        ADD FULLTEXT(vacancy_title)
-    "
-     */
-    @Modifying
-    @Query( value = "SELECT * FROM offer WHERE MATCH (vacancy_title) AGAINST (:q)",
-            nativeQuery = true)
-    List<Offer> findAllByVacancyTitleContains(@Param("q") String q);
-
-    /*
-    MySql request
-    "MATCH () AGAINST ()" only work if vacancy_title have FULLTEXT
-    else need start the request -
-    "
-        ALTER TABLE offer
-        ADD FULLTEXT(vacancy_title)
-    "
-     */
-    @Query( value = "SELECT * FROM offer WHERE MATCH (vacancy_title) AGAINST (:q)",
-            countQuery = "SELECT count(*) FROM offer WHERE MATCH (vacancy_title) AGAINST (:q)",
-        nativeQuery = true)
-    List<Offer> findAllByVacancyTitleContains(@Param("q") String q, Pageable pageable);
 }
