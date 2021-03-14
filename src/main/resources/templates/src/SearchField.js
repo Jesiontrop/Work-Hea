eaf02c96e6bc0d51a6cc089ab5f90c6f26cb0322
Andrey Barkov <andrey.ru754@gmail.com>
2021-03-14 12:20:09 +0300

    feat: add search-field element

---
+++ src/main/resources/templates/src/SearchField.js
@@ -0,0 +1,35 @@
+import React from "react";
+import Button from "./Button";
+import Select from "./Select";
+import styles from "./SearchField.module.scss";
+
+class SearchField extends React.Component {
+    constructor(props) {
+        super(props);
+        if (props.text)
+            this.text = props.text;
+        else
+            this.text = "Search";
+        this.data = props.data;
+        if (props.onButtonClick)
+            this.onButtonClick = props.onButtonClick;
+        else
+            this.onButtonClick = function () {};
+
+        this.onButtonClick = this.onButtonClick.bind(this);
+    }
+
+    render() {
+        return (
+            <div className={styles.search}>
+                <div className={styles.flexWrapperOne}>
+                    <input className={styles.field} />
+                    <Button onClick={this.onButtonClick} text={this.text} />
+                </div>
+                <Select data={this.props.data}/>
+            </div>
+        );
+    }
+}
+
+export default SearchField;
