91c1be4e1965a203ebfd0b4f29b1cb5024cdba06
Andrey Barkov <andrey.ru754@gmail.com>
2021-03-15 22:35:07 +0300

    feat: add index element

---
+++ src/main/resources/templates/src/index.js
@@ -0,0 +1,36 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Button from "./Button";
+import SearchField from "./SearchField";
+import styles from "./Index.module.scss";
+
+class Index extends React.Component {
+    constructor(props) {
+        super(props);
+        this.options = ["organization", "offer"];
+        this.grayColor = {background: "#BDBDBD"}
+    }
+
+    render() {
+        return (
+            <div className={styles.main}>
+                <div className={styles.head}>
+                    <div className={styles.login}>
+                        <div className={styles.button}>
+                            <Button version="second" text="Sign In"/>
+                        </div>
+                        <div className={styles.button}>
+                            <Button style={this.grayColor} version="second" text="Sign Up"/>
+                        </div>
+                    </div>
+                </div>
+                <div className={styles.body}>
+                    <p className={styles.workHea}>Work Hea</p>
+                    <SearchField options={this.options}/>
+                </div>
+            </div>
+        );
+    }
+}
+
+ReactDOM.render(<Index />, document.getElementById("root"));
