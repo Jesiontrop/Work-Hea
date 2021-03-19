5a73aca99354b9a422962ed3b1294b76a4da2a21
Andrey Barkov <andrey.ru754@gmail.com>
2021-03-19 22:39:10 +0300

    fix: fix name of table in jpa

--- src/main/java/com/jesiontrop/workhea/security/model/User.java
+++ src/main/java/com/jesiontrop/workhea/security/model/User.java
@@ -8,14 +8,11 @@ import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.util.Arrays;
 import java.util.Collection;
 
-@Entity
+@Entity(name = "\"user\"")
 @Data
 @NoArgsConstructor(access = AccessLevel.PRIVATE, force = true)
 @RequiredArgsConstructor
