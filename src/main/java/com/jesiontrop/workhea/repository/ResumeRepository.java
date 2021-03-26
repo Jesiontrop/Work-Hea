d20146872b6f53816de6fca6710ba547f8991d53
Jusepa <andrey.ru754@gmail.com>
2021-03-26 21:59:49 +0300

    refactor: change CrudRepostitory to PagingAndSortingRepository

--- src/main/java/com/jesiontrop/workhea/repository/ResumeRepository.java
+++ src/main/java/com/jesiontrop/workhea/repository/ResumeRepository.java
@@ -2,6 +2,7 @@ package com.jesiontrop.workhea.repository;
 
 import com.jesiontrop.workhea.model.Resume;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 
-public interface ResumeRepository extends CrudRepository<Resume, Long> {
+public interface ResumeRepository extends PagingAndSortingRepository<Resume, Long> {
 }
