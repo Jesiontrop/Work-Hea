f4d9c3de2c965fa2afb711721c01388aaf931ce9
Jusepa <andrey.ru754@gmail.com>
2021-03-26 22:02:25 +0300

    refactor: change CrudRepostitory to PagingAndSortingRepository

--- src/main/java/com/jesiontrop/workhea/repository/OfferRepository.java
+++ src/main/java/com/jesiontrop/workhea/repository/OfferRepository.java
@@ -1,13 +1,7 @@
 package com.jesiontrop.workhea.repository;
 
 import com.jesiontrop.workhea.model.Offer;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.repository.query.Param;
+import org.springframework.data.repository.PagingAndSortingRepository;
 
-import java.util.List;
-
-public interface OfferRepository extends CrudRepository<Offer, Long> {
+public interface OfferRepository extends PagingAndSortingRepository<Offer, Long> {
 }
