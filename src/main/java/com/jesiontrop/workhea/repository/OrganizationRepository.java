5e0bc172228fd10304245142dedd4612de3f061a
Jusepa <andrey.ru754@gmail.com>
2021-03-26 22:02:30 +0300

    refactor: change CrudRepostitory to PagingAndSortingRepository

--- src/main/java/com/jesiontrop/workhea/repository/OrganizationRepository.java
+++ src/main/java/com/jesiontrop/workhea/repository/OrganizationRepository.java
@@ -1,13 +1,12 @@
 package com.jesiontrop.workhea.repository;
 
 import com.jesiontrop.workhea.model.Organization;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.repository.PagingAndSortingRepository;
 
 import java.util.List;
 
-@RepositoryRestResource(collectionResourceRel = "organizations", path = "organizations")
-public interface OrganizationRepository extends CrudRepository<Organization, Long> {
+public interface OrganizationRepository extends PagingAndSortingRepository<Organization, Long> {
 
     List<Organization> findAllByNameOfOrganizationContains(String q);
+    
 }
