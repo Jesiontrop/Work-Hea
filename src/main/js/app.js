d1f499f2ea4962caedf2431cbc0e02b0101c9f93
Jusepa <andrey.ru754@gmail.com>
2021-04-07 22:20:00 +0300

    refactor: move Home page to file

--- src/main/js/app.js
+++ src/main/js/app.js
@@ -1,38 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import {Button} from "./modules/Button";
-import {SearchField} from "./modules/SearchField";
-import styles from "./styles/Index.module.scss";
+import { BrowserRouter, Route } from "react-router-dom";
+import {Home} from "./Home";
 
 class App extends React.Component {
     constructor(props) {
         super(props);
-        this.options = ["organization", "offer"];
-        this.grayColor = {background: "#BDBDBD"}
     }
 
     render() {
         return (
-            <div className={styles.main}>
-                <div className={styles.head}>
-                    <div className={styles.login}>
-                        <div className={styles.button}>
-                            <Button version="second" text="Sign In"/>
-                        </div>
-                        <div className={styles.button}>
-                            <Button style={this.grayColor} version="second" text="Sign Up"/>
-                        </div>
-                    </div>
-                </div>
-                <div className={styles.body}>
-                    <p className={styles.workHea}>Work Hea</p>
-                    <SearchField options={this.options}/>
-                </div>
-            </div>
+            <BrowserRouter >
+                <Route exact path="/" component={Home}/>
+            </BrowserRouter>
         );
     }
 }
-
-export default App;
 
 ReactDOM.render(<App />, document.getElementById("root"));
